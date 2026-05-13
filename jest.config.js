602c7dc4b255153b83805049c0dcfeb3f752cc36
yangyag <yangyag@hanmail.net>
2026-05-13 15:35:48 +0900

    feat: Expo 프로젝트 초기 셋업

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'jest-expo',
+  setupFilesAfterEach: ['<rootDir>/jest.setup.ts'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|expo(nent)?|@expo(nent)?/.*|expo-modules-core|expo-router|expo-secure-store|@unimodules/.*|sentry-expo|native-base|react-clone-referenced-element)',
+  ],
+  testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+};
